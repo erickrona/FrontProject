a6381d7a5c928f928dec5ea95cae4eee2cb34cb7
erickrona <49962019+erickrona@users.noreply.github.com>
2019-12-02 16:19:44 -0600

    Add files via upload

---
+++ js/signup.js
@@ -0,0 +1,37 @@
+
+$('#signup_button').on('click', function(){
+  // cargar los valores de password, email, name, age
+  let password = $('#password').val()
+  let email = $('#email').val()
+  let age = $('#age').val()
+  let name = $('#name').val()
+  
+  json_to_send = {
+    "password" : password,
+    "email": email,
+    "name": name,
+    "age": age
+  };
+
+  json_to_send = JSON.stringify(json_to_send);
+
+  $.ajax({
+    //url: 'http://localhost:3000/users',
+    url: 'https://todolist-finalproject.herokuapp.com/users',
+    headers: {
+        'Content-Type':'application/json'
+    },
+    method: 'POST',
+    dataType: 'json',
+    data: json_to_send,
+    success: function(data){
+      alert("Usuario creado con exito");
+      console.log('success: '+ data);
+      window.location = './index.html'
+    },
+    error: function(error_msg) {
+      alert((error_msg['responseText']));
+    }
+  });
+
+});
---
+++ js/todo.js
@@ -0,0 +1,93 @@
+var token = localStorage.getItem('token');
+if (token) {
+  token = token.replace(/^"(.*)"$/, '$1'); // Remove quotes from token start/end.
+}
+
+
+var todos = document.querySelectorAll("input[type=checkbox]");
+
+function loadTodos() {
+  $.ajax({
+    //url: 'http://localhost:3000/todos',
+    url: 'https://todolist-finalproject.herokuapp.com/todos',
+    headers: {
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer ' + token
+    },
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+      console.log(data)
+
+      for( let i = 0; i < data.length; i++) {
+        // aqui va su código para agregar los elementos de la lista
+        console.log(data[i].description)
+        // algo asi:
+        // addTodo(data[i]._id, data[i].description, data[i].completed)
+        // no tienen que usar la funcion de addTodo, es un ejemplo
+        let newElement = document.createElement("li");
+        newElement.classList.add("todo");
+        newElement.textContent = data[i].description;
+        if(data[i].completed){
+          newElement.classList.remove("todo");
+          newElement.classList.add("done");
+        }
+        $("#todo-list").append(newElement);
+        
+      }
+    },
+    error: function(error_msg) {
+      alert((error_msg['responseText']));
+    }
+  });
+}
+
+loadTodos()
+
+
+// o con jquery
+// $('input[name=newitem]').keypress(function(event){
+//     var keycode = (event.keyCode ? event.keyCode : event.which);
+//     if(keycode == '13'){
+//         $.ajax({})
+//     }
+// });
+
+var input = document.querySelector("input[name=newitem]");
+
+input.addEventListener('keypress', function (event) {
+  if (event.charCode === 13) {
+    json_to_send = {
+      "description" : input.value
+    };
+    json_to_send = JSON.stringify(json_to_send);
+    $.ajax({
+      //url: 'http://localhost:3000/todos',
+      url: 'https://todolist-finalproject.herokuapp.com/todos',
+      headers: {
+          'Content-Type':'application/json',
+          'Authorization': 'Bearer ' + token
+      },
+      method: 'POST',
+      dataType: 'json',
+      data: json_to_send,
+      success: function(data){
+        console.log(data)
+        // agregar código aqui para poner los datos del todolist en el el html
+        let newElement = document.createElement("li");
+        newElement.classList.add("todo");
+        newElement.textContent = data.description;
+        $("#todo-list").append(newElement);
+      },
+      error: function(error_msg) {
+        alert((error_msg['responseText']));
+      }
+    });
+    input.value = '';
+  }
+})
+
+
+function addTodo(id, todoText, completed) {
+  
+}
